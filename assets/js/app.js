8ac12962e519e3e374c97279999f2fd796e6b9e5
Mattia A. Fritz <mattia.fritz@unige.ch>
2021-02-20 21:09:37 +0100

    Corrected bug with activities

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,21 @@
-// Init local storage
-let db = window.localStorage;
+// If it is the page about the programme
+if (window.location.pathname.includes("programme.html")) {
+  // Init local storage
+  let db = window.localStorage;
 
-// Get all activities
-let activities = document.querySelectorAll("input.form-check-input");
+  // Get all activities
+  let activities = document.querySelectorAll("input.form-check-input");
 
-// Iterate through activities
-activities.forEach(function (el) {
-  //Add listener to checkboxes
-  el.addEventListener("click", function (event) {
-    let checkbox = event.target;
-    db.setItem(event.target.id, event.target.checked);
+  // Iterate through activities
+  activities.forEach(function (el) {
+    //Add listener to checkboxes
+    el.addEventListener("click", function (event) {
+      let checkbox = event.target;
+      // Set the status for the item (must be transformed because local storage stores strings)
+      db.setItem(event.target.id, Number(event.target.checked));
+    });
+
+    //Define if it is checked or not (must be transformed because local storage stores strings)
+    el.checked = Boolean(Number(db.getItem(el.id)));
   });
-
-  //Define if it is checked or not
-  el.checked = db.getItem(el.id);
-});
+}
